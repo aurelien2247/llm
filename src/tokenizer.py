0ccb276a0f8896a00bb432095a11d602fa1a84b4
Moignet <moignet@MacBook-Air-de-Moignet.local>
2025-11-24 13:03:54 +0100

    feat: Ajout d'un GPTDataset et d'un dataloader

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,16 +1,74 @@
 import importlib
 import tiktoken
+import torch
 
+# Utilisation d'un tokenizer BPE
 tokenizer = tiktoken.get_encoding("gpt2")
-text = (
-    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-     "of someunknownPlace."
+
+
+with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+
+context_size = 4 
+enc_sample = enc_text[50:]
+
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+
+from torch.utils.data import Dataset, DataLoader
+
+def create_dataloader(txt, batch_size=4, max_length=256, 
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+
+    # Initialisation du tokenizer
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    # Créer le dataset
+    dataset = GPTDataset(txt, tokenizer, max_length, stride)
+
+    # Créeer le dataloader
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+
+class GPTDataset(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # Tokenize the entire text
+        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+        # Use a sliding window to chunk the book into overlapping sequences of max_length
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
+dataloader = create_dataloader(
+    raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
 )
 
-integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+dataloader = create_dataloader(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
 
-print(integers)
-
-strings = tokenizer.decode(integers)
-
-print(strings)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Inputs:\n", inputs)
+print("\nTargets:\n", targets)
