7c26cf6f782f6ba9be65d311e6437ff37b01c564
Moignet <moignet@MacBook-Air-de-Moignet.local>
2025-12-01 13:47:25 +0100

    feat: creation de l'architecture du projet llm

---
+++ src/gpt-architecture/generate.py
@@ -0,0 +1,16 @@
+import torch
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (B, T) array of indices in the current context
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
